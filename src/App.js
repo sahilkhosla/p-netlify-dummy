28746e68d5ff8720e442f11ddab90a8928aae049
Sahil Khosla <sahilkhosla@gmail.com>
2022-08-29 14:46:18 -0400

    update message on homepage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          JS Kickstart Live Lessons
         </a>
       </header>
     </div>
